35d1e6b3c80de7bd46bdd496e2e50af55a893670
Денис <garaevdenis2019@gmail.com>
2023-05-22 20:30:21 +0500

    initial commit

---
+++ src/main/java/com/skypro/homework28/EmployeeNotFoundException.java
@@ -0,0 +1,14 @@
+package com.skypro.homework28;
+
+public class EmployeeNotFoundException extends RuntimeException{
+    public EmployeeNotFoundException() {
+    }
+
+    public EmployeeNotFoundException(String message) {
+        super(message);
+    }
+
+    public EmployeeNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
