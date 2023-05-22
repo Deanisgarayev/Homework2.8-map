7d33891adb3e6037f9debfa85bf736dc1566b9c0
Денис <garaevdenis2019@gmail.com>
2023-05-22 20:49:50 +0500

    initial commit

--- src/main/java/com/skypro/homework28/EmployeeBook.java
+++ src/main/java/com/skypro/homework28/EmployeeBook.java
@@ -1,4 +1,50 @@
 package com.skypro.homework28;
 
+import java.util.Objects;
+
 public class EmployeeBook {
+    package com.skypro.homework28;
+
+import java.util.Objects;
+
+    public class Employee {
+
+        private String firstname;
+
+        private String surname;
+
+
+        public Employee(String firstname, String surname) {
+            this.firstname = firstname;
+            this.surname = surname;
+        }
+
+        public String getFirstname() {
+            return this.firstname;
+        }
+        public String getSurname() {
+            return this.surname;
+        }
+
+
+        @Override
+        public String toString() {
+            return firstname + " Имя " + surname + "фамилия";
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Employee employee = (Employee) o;
+            return firstname.equals(employee.firstname) && surname.equals(employee.surname);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(firstname, surname);
+        }
+
+    }
+
 }
