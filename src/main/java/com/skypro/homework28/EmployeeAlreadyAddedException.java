93540a4f6e8580b8ffc49ab41870694a41b19127
Денис <garaevdenis2019@gmail.com>
2023-05-22 20:35:27 +0500

    initial commit

---
+++ src/main/java/com/skypro/homework28/EmployeeAlreadyAddedException.java
@@ -0,0 +1,14 @@
+package com.skypro.homework28;
+
+public class EmployeeAlreadyAddedException extends RuntimeException{
+    public EmployeeAlreadyAddedException() {
+    }
+
+    public EmployeeAlreadyAddedException(String message) {
+        super(message);
+    }
+
+    public EmployeeAlreadyAddedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
